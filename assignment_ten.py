c3629e42cb3f2d57292ea993b6cd9a690a32ed19
Yerim <yerim@iMac6.local>
2018-12-11 11:18:38 -0500

    I finished my project by adding the completed document strings

--- assignment_ten.py
+++ assignment_ten.py
@@ -1,3 +1,11 @@
+# Yerim Dia
+# Introduction to Computer Science
+# December 11, 2018
+# This program is a way of drawing with Pygame
+# The purpose of the program was to draw different rows of bricks with each row a different color and the next row has
+#  2 less bricks than the previous one.
+
+
 import pygame
 import sys
 from pygame.locals import *
@@ -17,26 +25,27 @@ number_bricks = 9
 
 pygame.init()
 mainSurface = pygame.display.set_mode((500, 250), 0, 32)
+mainSurface.fill((178, 34, 34))
 pygame.display.set_caption("Layer Bricks")
 
 
 WIDTH = (500 - (number_bricks * SPACE)) / number_bricks
+# This allows the bricks to evenly fit in the window
 
 x = 0
 y = 250 - HEIGHT
 
 for c in range(5):
     color = colors[c]
-    x = (WIDTH + SPACE) * c
+    x = (WIDTH + SPACE) * c  # Sets the indented x position for new brick after each row
     for b in range(number_bricks):
         bricks = brick.Brick(WIDTH, HEIGHT, color, mainSurface)
         bricks.create_a_brick(x, y)
-        x = x + WIDTH + SPACE
+        x = x + WIDTH + SPACE  # Sets the next brick in a row
+
         pygame.display.update()
-    number_bricks = number_bricks - 2
-    y = y - HEIGHT - SPACE
-
-
+    number_bricks = number_bricks - 2  # each line of brick has 2 less bricks than previous
+    y = y - HEIGHT - SPACE  # Sets y coordinate for each brick on next row
 
 while True:
     for event in pygame.event.get():
--- brick.py
+++ brick.py
@@ -1,13 +1,26 @@
 import pygame
 
+
 class Brick:
     def __init__(self, width, height, color, main_surface):
+        """
+        This function describes how a brick is made and what parameters areinvolved to create a brick
+        :param width: It knows that it will include a width parameter to draw the bricks
+        :param height: It knows that it will include a height parameter to draw the bricks
+        :param color: It will require a color to draw a brick
+        :param main_surface: The screen or display where pygame runs
+        """
         self.main_surface = main_surface
         self.color = color
         self.height = height
         self.width = width
 
     def create_a_brick(self, x, y):
+        """
+        This function describes the positional arguments of the bricks
+        :param x: The x coordinate of the first brick
+        :param y: The y coordinate if the first brick
+        :return: none
+        """
         pygame.draw.rect(self.main_surface, self.color, (x, y, self.width, self.height), 0)
         pygame.display.update()
-    
